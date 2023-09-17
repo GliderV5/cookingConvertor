ad31cdf6f673390a0f452b2440e3e02655a58ad3
Aurelien Dubois <gliderperso@gmail.com>
2023-09-17 16:52:15 +0200

    FOnctionne bien

--- compile.py
+++ compile.py
@@ -1,7 +1,11 @@
 import os
 os.system("clear")
-os.system("javac -d bin src/*.java")
+os.system("javac -d bin src/script/*.java")
 print("")
 print("######## Lancement du programme ########")
 print("")
 os.system('java -cp bin/ script.Cuisine')
+print()
+if input("creat a jar file ? (y/n) : ") == "y":
+    os.system("jar cvfm cookingConvertor.jar manifest.txt -C bin/ .")
+    print("jar file created")
--- src/script/Cuisine.java
+++ src/script/Cuisine.java
@@ -10,10 +10,20 @@ public class Cuisine {
 
         // Demander la quantité initiale
         String input = JOptionPane.showInputDialog("Quantité initiale : ");
+        // Vérifier si l'utilisateur a cliqué sur annuler ou sur la croix
+        if (input == null || input.equals(JOptionPane.CANCEL_OPTION) || input.equals(JOptionPane.CLOSED_OPTION)) {
+            // Terminer le programme
+            System.exit(0);
+        }
         double init = Double.parseDouble(input);
 
         // Demander la quantité finale
         input = JOptionPane.showInputDialog("Quantité finale : ");
+        // Vérifier si l'utilisateur a cliqué sur annuler ou sur la croix
+        if (input == null || input.equals(JOptionPane.CANCEL_OPTION) || input.equals(JOptionPane.CLOSED_OPTION)) {
+            // Terminer le programme
+            System.exit(0);
+        }
         double dest = Double.parseDouble(input);
 
         // Entrer dans une boucle infinie
@@ -21,6 +31,11 @@ public class Cuisine {
             try {
                 // Demander la valeur d'un autre ingrédient
                 input = JOptionPane.showInputDialog("Entrez maintenant la valeur d'un autre ingrédient : ");
+                // Vérifier si l'utilisateur a cliqué sur annuler ou sur la croix
+                if (input == null || input.equals(JOptionPane.CANCEL_OPTION) || input.equals(JOptionPane.CLOSED_OPTION)) {
+                    // Terminer le programme
+                    System.exit(0);
+                }
                 double valeur = Double.parseDouble(input);
 
                 // Calculer et afficher le résultat
